3b4394fd8842c857d2bebdd13aa8f68696ea9756
Syed Abdul Haseeb <syedabdulhaseeb611@gmail.com>
2025-06-01 02:18:39 +0500

    Refactor: Enhance docstrings for password verification, hashing, and image upload functions

--- backend/utils/main.py
+++ backend/utils/main.py
@@ -15,16 +15,33 @@ cloudinary.config(
 
 
 def verify_password(plain_password:str, hashed_password:str):
+    """Verifies a plain password against a hashed password.
+    Args:
+        plain_password (str): The plain password to verify.
+        hashed_password (str): The hashed password to compare against.
+    Returns:
+        bool: True if the passwords match, False otherwise.
+    """
     return bcrypt.checkpw(plain_password.encode(), hashed_password.encode())
-    # return pwd_context.verify(plain_password, hashed_password)
 
 
 def get_password_hash(password:str):
-    # return pwd_context.hash(password)
+    """Hashes a password using bcrypt.
+    Args:
+        password (str): The password to hash.
+    Returns:
+        str: The hashed password.
+    """
     salt = bcrypt.gensalt()
     return bcrypt.hashpw(password.encode(), salt).decode()
 
 async def upload_image(file) -> Image:
+    """Uploads an image to Cloudinary and returns an Image object.
+    Args:
+        file: The file to upload, typically a binary file or a file-like object.
+    Returns:
+        Image: An Image object containing the details of the uploaded image.
+    """
     uploaded = cloudinary.uploader.upload(file, resource_type="image", folder="auction_x")
     fields = ["asset_id", "public_id", "width", "height", "resource_type", "tags", "url", "secure_url"]
     for field in fields:
