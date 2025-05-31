be4b75528a8399281fcb07e749e8ef13d6b91bd1
Syed Abdul Haseeb <syedabdulhaseeb611@gmail.com>
2025-06-01 02:18:24 +0500

    Refactor: Add validation constraints for full_name, username and password fields in UserCreate model

--- backend/schemas/users.py
+++ backend/schemas/users.py
@@ -36,11 +36,11 @@ class GoogleUser(BaseModel):
     token: GoogleToken
 
 class UserCreate(BaseModel):
-    full_name: str = Field(examples=["John Doe"])
-    username: str = Field(examples=["john_doe"])
+    full_name: str = Field(examples=["John Doe"], min_length=3, max_length=80)
+    username: str = Field(examples=["john_doe"], min_length=3, max_length=50)
     email: EmailStr
     email_verified: Optional[bool] = False
-    password: str
+    password: str = Field(min_length=6, max_length=128)
     avatar: Optional[Union[Image, str]] = None
     cover_image: Optional[Union[Image, str]] = None
     account_type: Optional[str] = Field(default="EMAIL")
