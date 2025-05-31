d5cfebb7b1055764350e26652a54cc5411d0386c
Syed Abdul Haseeb <syedabdulhaseeb611@gmail.com>
2025-06-01 02:16:34 +0500

    Refactor: Update listings module to use asynchronous database operations and ensure slug index creation

--- backend/db/listings.py
+++ backend/db/listings.py
@@ -1,47 +1,59 @@
 from ..utils.db import get_database
 from . import users
 from bson import ObjectId
-from pymongo import ReturnDocument
+from pymongo import ReturnDocument, ASCENDING
+from pymongo.asynchronous.database import AsyncDatabase
+from pymongo.errors import OperationFailure
 
-db = get_database()
-collection = db["listings"]
 
-collection.create_index("slug", unique=True)
+# db.listings.create_index("slug", unique=True)
 
-async def get_listings():
-    listing_list = collection.find()
-    return list(listing_list)
+async def ensure_slug_index(db: AsyncDatabase):
+    existing_indexes = [index["name"] async for index in await db.listings.list_indexes()]
+    if "slug_1" not in existing_indexes:
+        try:
+            await db.listings.create_index([("slug", ASCENDING)], name="slug_1", unique=True)
+            print("✅ Created index on 'slug'")
+        except OperationFailure as e:
+            print(f"⚠️ Index creation failed: {e}")
 
-async def create_listing(listing: dict):
+async def get_listings(db: AsyncDatabase):
+    listing_list = []
+    db.listings.list_indexes()
+    async for listing in db.listings.find():
+        listing_list.append(listing)
+    return listing_list
+
+async def create_listing(listing: dict, db: AsyncDatabase):
     try:
         owner_id = listing["owner"]
-        owner = await users.get_user_by_id(owner_id)
+        owner = await users.get_user_by_id(owner_id, db)
         if not owner:
             raise ValueError("owner does not exist")
-        result = collection.insert_one(listing)
+        result = await db.listings.insert_one(listing)
         try:
-            await users.update_listing(owner["username"], result.inserted_id)
+            await users.update_listing(owner["username"], result.inserted_id, db)
         except Exception as e:
-            collection.delete_one({"_id": result.inserted_id})
+            await db.listings.delete_one({"_id": result.inserted_id})
             raise e
     except KeyError:
         raise ValueError("owner is required")
     return result.inserted_id
 
-async def get_listing_by_slug(slug: str):
-    listing = collection.find_one({"slug": slug})
+async def get_listing_by_slug(slug: str, db: AsyncDatabase):
+    listing = await db.listings.find_one({"slug": slug})
     return listing
 
-async def update_listing(slug: str, data: dict, new: bool = False):
-    return collection.find_one_and_update({"slug": slug}, {"$set": data}, return_document=ReturnDocument.AFTER if new else ReturnDocument.BEFORE)
+async def update_listing(slug: str, data: dict, db: AsyncDatabase, new: bool = False):
+    return await db.listings.find_one_and_update({"slug": slug}, {"$set": data}, return_document=ReturnDocument.AFTER if new else ReturnDocument.BEFORE)
 
-async def update_listing_bid(slug: str, data: dict, new:bool = False):
-    return collection.find_one_and_update({"slug": slug}, {"$push": {"bids": data}, "$set": {"current_bid": data["amount"]}}, return_document=ReturnDocument.AFTER if new else ReturnDocument.BEFORE)
+async def update_listing_bid(slug: str, data: dict, db: AsyncDatabase, new:bool = False):
+    return await db.listings.find_one_and_update({"slug": slug}, {"$push": {"bids": data}, "$set": {"current_bid": data["amount"]}}, return_document=ReturnDocument.AFTER if new else ReturnDocument.BEFORE)
     
 
-async def delete_listing(username: str, listing_slug: str):
-    listing = collection.find_one({"slug": listing_slug})
-    owner = await users.get_user_by_username(username)
+async def delete_listing(username: str, listing_slug: str, db: AsyncDatabase):
+    listing = await db.listings.find_one({"slug": listing_slug})
+    owner = await users.get_user_by_username(username, db)
     if str(listing["owner"]) != str(owner["_id"]):
         raise ValueError("listing does not belong to user")
-    return collection.delete_one({"slug": listing_slug})
+    return await db.listings.delete_one({"slug": listing_slug})
