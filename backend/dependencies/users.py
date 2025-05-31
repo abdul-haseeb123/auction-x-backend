dc767ee325ce2dd04cc741b00c413a6386a4b867
Syed Abdul Haseeb <syedabdulhaseeb611@gmail.com>
2025-06-01 02:17:37 +0500

    Refactor: Update get_current_user function to use asynchronous database operations as a dependency

--- backend/dependencies/users.py
+++ backend/dependencies/users.py
@@ -1,12 +1,14 @@
 import jwt
 import os
 from jwt.exceptions import InvalidTokenError
-from fastapi import Request, HTTPException
+from fastapi import Request, HTTPException, Depends
 from ..schemas import User
 from ..db import users
 from datetime import datetime
+from pymongo.asynchronous.database import AsyncDatabase
+from .db import get_db
 
-async def get_current_user(request: Request) -> User:
+async def get_current_user(request: Request, db:AsyncDatabase = Depends(get_db)) -> User:
     credentials_exception = HTTPException(
         status_code=401,
         detail="Could not validate credentials",
@@ -20,7 +22,7 @@ async def get_current_user(request: Request) -> User:
         token = request.headers.get("Authorization").split(" ")[1]
     if request.cookies.get("access_token") != None:
         token = request.cookies.get("access_token")
-    google_user = await users.get_google_user_by_access_token(token)
+    google_user = await users.get_google_user_by_access_token(token, db)
     if google_user:
         print("google user found")
         if google_user["token"]["expires_at"] < datetime.now():
@@ -34,7 +36,7 @@ async def get_current_user(request: Request) -> User:
             raise credentials_exception
     except InvalidTokenError:
         raise credentials_exception
-    user = await users.get_user_by_username(username)
+    user = await users.get_user_by_username(username, db)
     if user is None:
         raise credentials_exception
     return User(**user)
