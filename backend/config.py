785d869ce313657bca64dfab7e8252d66d7e2fb3
Syed Abdul Haseeb <syedabdulhaseeb611@gmail.com>
2025-06-17 02:11:19 +0500

    Refactor Settings class to set default values for MongoDB and OAuth2 fields

--- backend/config.py
+++ backend/config.py
@@ -1,10 +1,11 @@
+from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    mongodb_uri: str
-    db_name: str
-    test_db_name: str
+    mongodb_uri: str = Field(default="mongodb://localhost:27017")
+    db_name: str = Field(default="commerce")
+    test_db_name: str = Field(default="test-commerce")
 
     access_token_secret: str
     access_token_expiry: int
@@ -18,7 +19,7 @@ class Settings(BaseSettings):
     cloudinary_api_key: str
     cloudinary_api_secret: str
 
-    oauth2_refresh_token_generator: bool
+    oauth2_refresh_token_generator: bool = Field(default=True)
 
     model_config = SettingsConfigDict(env_file=".env")
 
