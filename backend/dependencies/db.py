d856e0e796715af67e54a0d453f4f6608f19e514
Syed Abdul Haseeb <syedabdulhaseeb611@gmail.com>
2025-06-17 01:58:41 +0500

    Refactor: removed unnecessary code

--- backend/dependencies/db.py
+++
@@ -1,9 +0,0 @@
-from pymongo import AsyncMongoClient
-import os
-
-client = AsyncMongoClient(os.environ.get("MONGODB_URI", "mongodb://localhost:27017/"))
-
-
-def get_db():
-    db = client[os.environ.get("DB_NAME", "commerce")]
-    yield db
